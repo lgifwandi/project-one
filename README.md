30fe3ed04fb532dbc8f5e40fcdae0c7e1c793c11
Lee Berny Gifwandi <leesmacbookpro@macbook-pro.myfiosgateway.com>
2021-09-19 21:02:25 -0400

    Project4

---
+++ README.md
@@ -0,0 +1,47 @@
+Five recents crime reported
+
+It's an application that displays five recents crime reported in New York city by typing the zip code.
+
+☐ Technologies Used
+
+.HTML
+.CSS
+.JavaScript
+.jQuery
+.Hosted on Vercel
+.AJAX used to receive data from ana external source NYPDapi.
+.NYPD API
+
+☐ Screenshot
+
+
+
+
+
+
+
+☐ Getting Started
+
+
+
+
+
+
+
+☐ Future Enhancements
+
+.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,40 +1,70 @@
 const $zipCode = $("#incident_zip");
 const $time  = $("#created_date");
-const $complaint = $("#descriptor");
+const $complaint = $("#complaint_type");
 const $status = $("#status");
 const $text = $("#text");
+const $mainContent = $('main');
+
+let policeData;
 
 function getData(evt){
+
   evt.preventDefault();
 
+//prompt a user to input the zip code
 userInput = $text.val();
+
 
 $.ajax({
     url: `https://data.cityofnewyork.us/resource/erm2-nwe9.json?incident_zip=${userInput}`
   }).then(
     (openData) => {
     
+    policeData = openData;
+    //console.log(policeData);
       
-      policeData = openData;
-      recentPoliceData = policeData.slice(0,4);
-      render();
+    parseData()
+    render();
     },
     (error) => {
+
       console.log("Oops something went wrong: ", error);
+
     }
   );
 }
+function parseData() {
 
-function render(){
+  //A function to sort an array to get the recent five complaints
+  let sortedArray = policeData.sort(function(a, b){
 
-  for ( let i = 0; i < 5 ; i++ ) {
-     
-     $zipCode.text(recentPoliceData[i].incident_zip);
-     $time.text(recentPoliceData[i].created_date);
-     $complaint.text(recentPoliceData[i].descriptor);
-     $status.text(recentPoliceData[i].status);
+    return new Date(b.created_date) - new Date(a.created_date);
+  
+  });
 
-    }
+    
+  //Reduce the array to five index
+   return sortedArray.slice(0,5); 
+   
 }
 
+function render(){
+ 
+
+let shortenedArray = parseData();
+console.log(shortenedArray);
+
+
+//A forEach to display five recent complaints
+shortenedArray.forEach(function(incident){
+
+  let zipComplaint = $(`<p>incident_zip : ${incident.incident_zip}</p>`);
+  let timeComplaint = $(`<p>created_date :${incident.created_date}</p>`);
+  let typeComplaint = $(`<p>complaint_type :${incident.complaint_type}</p>`);
+  let statusComplaint = $(`<p>status :${incident.status}</p>`);
+
+  $mainContent.append(zipComplaint,timeComplaint,typeComplaint,statusComplaint);
+});
+
+}
 $('form').on('submit', getData)
